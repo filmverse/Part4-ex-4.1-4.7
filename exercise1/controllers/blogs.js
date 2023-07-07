75baf1577ff78427d21f2426cb2f53d02c5e63c0
Vikas Sharma <vkassharma19492@gmail.com>
2023-07-08 01:57:15 +0530

    structuring the backend

---
+++ exercise1/controllers/blogs.js
@@ -0,0 +1,20 @@
+const blogsRouter = require('express').Router()
+const blog = require('../models/blog')
+const Blog = require('../models/blog')
+
+
+blogsRouter('/', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs)
+    })
+})
+
+blogsRouter('/', (request, response) => {
+    const blog = new Blog(request.body)
+    blog.save().then(result => {
+        response.status(201).json(result)
+    })
+
+})
+
+module.exports = blogsRouter
